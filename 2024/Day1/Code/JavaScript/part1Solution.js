13e7558dad518165d546ef7da777d288e6a3559f
NicoMaker <nicola.marano02gmail,com>
2024-12-03 12:38:15 +0100

    ottimizazion code

--- 2024/Day1/Code/JavaScript/part1Solution.js
+++ 2024/Day1/Code/JavaScript/part1Solution.js
@@ -1,36 +1,38 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs"),
+  path = require("path");
 
 //Input file has to be created first
-const inputDay1 = fs.readFileSync(path.join(__dirname + '/inputDay1.txt'), 'utf-8')
+const inputDay1 = fs.readFileSync(
+    path.join(__dirname + "/inputDay1.txt"),
+    "utf-8"
+  ),
+  spllittedInput = inputDay1.split("\n"),
+  rawRow = spllittedInput[0].split(" ");
 
-const spllittedInput = inputDay1.split('\n')
+let lastElement = rawRow.length - 1,
+  leftList = [],
+  rightList = [];
 
-const rawRow = spllittedInput[0].split(' ')
-let lastElement = rawRow.length - 1
+spllittedInput.forEach((element) => {
+  let rawRow = element.split(" ");
+  leftList.push(parseInt(rawRow[0]));
+  rightList.push(parseInt(rawRow[lastElement]));
+});
 
-let leftList = []
-let rightList = []
-
-spllittedInput.forEach(element => {
-    let rawRow = element.split(' ')
-    leftList.push(parseInt(rawRow[0]))
-    rightList.push(parseInt(rawRow[lastElement]))
-})
-
-let orderedLeftList = leftList.sort()
-let orderedRightList = rightList.sort()
-
-let distanceList = []
-let distance = 0
+let orderedLeftList = leftList.sort(),
+  orderedRightList = rightList.sort(),
+  distanceList = [],
+  distance = 0;
 
 for (let i = 0; i < orderedLeftList.length; i++) {
-    distance = Math.abs(orderedLeftList[i] - orderedRightList[i])
-    if(isNaN(distance))
-        continue
-    distanceList.push(distance)
+  distance = Math.abs(orderedLeftList[i] - orderedRightList[i]);
+  if (isNaN(distance)) continue;
+  distanceList.push(distance);
 }
 
-let sum = distanceList.reduce((accumulativeSum, element) => accumulativeSum + element, 0) //0 is initial value
+let sum = distanceList.reduce(
+  (accumulativeSum, element) => accumulativeSum + element,
+  0
+); //0 is initial value
 
-console.log(sum)
+console.log(sum);
