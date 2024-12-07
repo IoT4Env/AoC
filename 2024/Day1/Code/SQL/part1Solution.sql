02540d425d11f7b8a3f67297d35424aa963a51bb
IoT4Env <ioan.matteo253@gmail.com>
2024-12-07 13:59:16 +0100

    Transformed input data into table-like data + updated requirements.md

--- 2024/Day1/Code/SQL/part1Solution.sql
+++ 2024/Day1/Code/SQL/part1Solution.sql
@@ -1,17 +1,15 @@
 BEGIN;
+    --Declare in-memory tables
     PRAGMA temp_store = 2;
 
-    CREATE TABLE IF NOT EXISTS _TEMP1 (
-        Col1 VARCHAR(10) NOT NULL
+    --Create table for storing total input rows
+    CREATE TABLE IF NOT EXISTS _ROW_COUNT (
+        Count VARCHAR(10) NOT NULL
     );
 
-    CREATE TABLE IF NOT EXISTS _TEMP2 (
-        Col1 INT NOT NULL,
-        Col2 INT NOT NULL
-    );
-
+    --Get puzzle input row count
     WITH
-        CTE (x, y) AS (
+        CTE (Iteration, RowCount) AS (
             SELECT 
                 1,
                 0
@@ -19,141 +17,51 @@ BEGIN;
             UNION ALL
 
             SELECT 
-                x + 1,
+                Iteration + 1,
                 CASE
-                    WHEN SUBSTRING(INPUT_DAY1.Input, x + 1, 1) = CHAR(10) THEN
-                        y + 1
-                    ELSE y
+                    WHEN SUBSTRING(INPUT_DAY1.Input, Iteration + 1, 1) = CHAR(10) THEN
+                        RowCount + 1
+                    ELSE RowCount
                 END 
             FROM CTE, INPUT_DAY1
-            WHERE x < LENGTH(INPUT_DAY1.Input)
+            WHERE Iteration < LENGTH(INPUT_DAY1.Input)
         )
-    INSERT INTO _TEMP1(
-        Col1
+    INSERT INTO _ROW_COUNT(
+        Count
     ) VALUES (
         (SELECT 
-            y
+            RowCount
         FROM CTE
-        ORDER BY Y DESC
+        ORDER BY RowCount DESC
         LIMIT 1)
     );
 
-    WITH
-        CTE(x, y) AS (
-            SELECT
+
+    --Create temporary table-like data structure inside below temporary table
+    WITH 
+        _DATA_TABLE(Iteration, LeftCol, RightCol, DataRow) AS (
+            SELECT 
                 1,
-                SUBSTRING(INPUT_DAY1.Input, 1, INSTR(INPUT_DAY1.Input, CHAR(10)) - 1)
+                CAST(SUBSTRING(INPUT_DAY1.Input, 1, INSTR(INPUT_DAY1.Input, CHAR(10))) AS INT),
+                CAST(SUBSTRING(INPUT_DAY1.Input, INSTR(INPUT_DAY1.Input, ' '), INSTR(INPUT_DAY1.Input, CHAR(10))) AS INT),
+                SUBSTRING(INPUT_DAY1.Input, 1, INSTR(INPUT_DAY1.Input, CHAR(10)))
             FROM INPUT_DAY1
 
             UNION ALL
 
             SELECT
-                x + 1,
-                SUBSTRING(INPUT_DAY1.Input, LENGTH(y) * x, INSTR(INPUT_DAY1.Input, CHAR(10)))
-            FROM INPUT_DAY1, CTE, _TEMP1
-            WHERE x < (SELECT 
-                    CAST(_TEMP1.Col1 AS INT)
-                FROM _TEMP1)
+                Iteration + 1,
+                CAST(SUBSTRING(INPUT_DAY1.Input, LENGTH(DataRow) * Iteration, INSTR(INPUT_DAY1.Input, CHAR(10))) AS INT),
+                CAST(SUBSTRING(INPUT_DAY1.Input, LENGTH(DataRow) * Iteration + INSTR(INPUT_DAY1.Input, ' '), INSTR(INPUT_DAY1.Input, CHAR(10))) AS INT),
+                SUBSTRING(INPUT_DAY1.Input, LENGTH(DataRow) * Iteration, INSTR(INPUT_DAY1.Input, CHAR(10)))
+            FROM _DATA_TABLE, INPUT_DAY1, _ROW_COUNT
+            WHERE Iteration < CAST(_ROW_COUNT.Count AS INT)
         )
-    INSERT INTO _TEMP2(
-        Col1, Col2
-    ) VALUES(
-        (SELECT
-            CAST(y AS INT)
-        FROM CTE),
-        (SELECT
-            CAST(SUBSTRING(y, LENGTH(SUBSTRING(y, 0, INSTR(y, ' '))) + 4, LENGTH(y)) AS INT)
-        FROM CTE)
-    );
+    SELECT
+        _DATA_TABLE.LeftCol,
+        _DATA_TABLE.RightCol
+    FROM _DATA_TABLE;
 
-    SELECT 
-        *
-    FROM _TEMP2;
-    -- SELECT
-    --     CAST(y AS INT),
-    --     CAST(SUBSTRING(y, LENGTH(SUBSTRING(y, 0, INSTR(y, ' '))) + 4, LENGTH(y)) AS INT)
-    -- FROM CTE;
 
-    DROP TABLE IF EXISTS _TEMP1;
-    DROP TABLE IF EXISTS _TEMP2;
+    DROP TABLE IF EXISTS _ROW_COUNT;
 END;
-
-
--- WITH
---     CTE (x, y) AS (
---         SELECT 
---             1,
---             0
-        
---         UNION ALL
-
---         SELECT 
---             x + 1,
---             CASE
---                 WHEN SUBSTRING(INPUT_DAY1.Input, x + 1, 1) = CHAR(10) THEN
---                     y + 1
---                 ELSE y
---             END 
---         FROM CTE, INPUT_DAY1
---         WHERE x < LENGTH(INPUT_DAY1.Input)
---     )
--- SELECT 
---     y
--- FROM CTE
--- ORDER BY Y DESC
--- LIMIT 1
-
--- WITH
---     CTE(x, y) AS (
---         SELECT
---             1,
---             SUBSTRING(INPUT_DAY1.Input, 1, INSTR(INPUT_DAY1.Input, CHAR(10)) - 1)
---         FROM INPUT_DAY1
-
---         UNION ALL
-
---         SELECT
---             x + 1,
---             SUBSTRING(INPUT_DAY1.Input, LENGTH(y) * x, INSTR(INPUT_DAY1.Input, CHAR(10)))
---         FROM INPUT_DAY1, CTE
---         WHERE x < 25
---     )
--- SELECT --DISTINCT
---     y
--- FROM CTE;
-
--- SELECT
---     --SUBSTRING(INPUT_DAY1.Input, 1, INSTR(INPUT_DAY1.Input, CHAR(10)) - 1) AS Col1,
---     SUBSTRING(INPUT_DAY1.Input, 1, LENGTH(INPUT_DAY1.Input) - INSTR(INPUT_DAY1.Input, CHAR(10)))
--- FROM INPUT_DAY1
-
-
--- SELECT
---     SUBSTRING(INPUT_DAY1.Input, 1, INSTR(INPUT_DAY1.Input, CHAR(10)) - 1) AS Col1,
---     SUBSTRING(INPUT_DAY1.Input, INSTR(INPUT_DAY1.Input, CHAR(10)), INSTR(INPUT_DAY1.Input, CHAR(10))) AS Col2
--- FROM INPUT_DAY1
-
-
-
--- WITH
---     CTE(x, y) AS (
---         SELECT
---             1,
---             SUBSTRING(INPUT_DAY1.Input, 0, 1)
---         FROM INPUT_DAY1
-
---         UNION ALL
-
---         SELECT
---             x + 1,
---             SUBSTRING(INPUT_DAY1.Input, x + 1, 1)
---             -- CASE
---             --     WHEN SUBSTRING(INPUT_DAY1.Input, x + 1, 1) = CHAR(10) THEN
---             --         y + 1
---             -- END
---         FROM INPUT_DAY1, CTE
---         WHERE x < 100
---     )
--- SELECT --DISTINCT
---     y
--- FROM CTE;
--- 2024/Day1/Code/SQL/requirements.md
+++ 2024/Day1/Code/SQL/requirements.md
@@ -1,11 +1,15 @@
 # SQL solution
 
-This solution requires sqlite3 installed in the PC.
-
-Verify the installation with this [command](#check-version)
-Any version of sqlite3 will do.
+## Premise
+A fex things before the instructions on how to execute sql solution for AoC day1:
+- Tables with prefix '_' are temporary tables;
+- This solution requires sqlite3 installed.
+    Verify the installation with this [command](#check-version)
+    sqlite3 version 3.8.3 or higher is required for CTE support.
 
 After checking all the above, you are ready to go:
+
+## Instructions
 
 1. Create a .db file inside the "SQL" folder like ```<db_file_name>.db```
 
@@ -18,14 +22,24 @@ After checking all the above, you are ready to go:
 ## sqlite3 commands
 
 ### Check version
-> sqlite3 --version
+```sh
+$ sqlite3 --version
+```
 
 ### Connect to lcoal .db file
-> sqlite3 <file_name>.db
+```sh
+$ sqlite3 <file_name>.db
+sqlite>
+```
 
-### Execute query inside the database
-> sqlite3 <file_name>.db < <query>.sql
+### Execute query from outside the database
+```sh
+$ sqlite3 <file_name>.db < part1Solution.sql
+```
 
 ### Quit sqlite3 session
-> .quit
+```sh
+sqlite> .quit
+$ 
+```
 
