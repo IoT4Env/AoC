3929d922dc921ebce01b23d1c6bf51c17413ce80
IoT4Env <ioan.matteo253@gmail.com>
2024-12-07 22:56:22 +0100

    Reduced sql query overload by storing input row count in persistent table

--- 2024/Day1/Code/SQL/part1Solution.sql
+++ 2024/Day1/Code/SQL/part1Solution.sql
@@ -2,43 +2,6 @@ BEGIN;
     --Declare in-memory tables
     PRAGMA temp_store = 2;
 
-    DROP TABLE IF EXISTS _ROW_COUNT;
-
-    --Create table for storing total input rows
-    CREATE TABLE IF NOT EXISTS _ROW_COUNT (
-        Count VARCHAR(10) NOT NULL
-    );
-
-    --Get puzzle input row count
-    WITH
-        CTE (Iteration, RowCount) AS (
-            SELECT 
-                1,
-                0
-            
-            UNION ALL
-
-            SELECT 
-                Iteration + 1,
-                CASE
-                    WHEN SUBSTRING(INPUT_DAY1.Input, Iteration + 1, 1) = CHAR(10) THEN
-                        RowCount + 1
-                    ELSE RowCount
-                END 
-            FROM CTE, INPUT_DAY1
-            WHERE Iteration < LENGTH(INPUT_DAY1.Input)
-        )
-    INSERT INTO _ROW_COUNT(
-        Count
-    ) VALUES (
-        (SELECT 
-            RowCount
-        FROM CTE
-        ORDER BY RowCount DESC
-        LIMIT 1)
-    );
-
-
     --Create temporary table-like data structure inside below temporary table
     WITH 
         _DATA_TABLE(Iteration, LeftCol, RightCol, DataRow) AS (
@@ -56,8 +19,8 @@ BEGIN;
                 CAST(SUBSTRING(INPUT_DAY1.Input, LENGTH(DataRow) * Iteration, INSTR(INPUT_DAY1.Input, CHAR(10))) AS INT),
                 CAST(SUBSTRING(INPUT_DAY1.Input, LENGTH(DataRow) * Iteration + INSTR(INPUT_DAY1.Input, ' '), INSTR(INPUT_DAY1.Input, CHAR(10))) AS INT),
                 SUBSTRING(INPUT_DAY1.Input, LENGTH(DataRow) * Iteration, INSTR(INPUT_DAY1.Input, CHAR(10)))
-            FROM _DATA_TABLE, INPUT_DAY1, _ROW_COUNT
-            WHERE Iteration < CAST(_ROW_COUNT.Count AS INT)
+            FROM _DATA_TABLE, INPUT_DAY1, ROW_COUNT
+            WHERE Iteration < ROW_COUNT.Count
         )
         SELECT
             SUM(ABS(_RIGHT_ASC.Value - _LEFT_ASC.Value)) AS TotalDistance
@@ -75,6 +38,4 @@ BEGIN;
         ) _RIGHT_ASC
         ON _RIGHT_ASC.Row = _LEFT_ASC.Row;
             
-
-    DROP TABLE IF EXISTS _ROW_COUNT;
 END;
