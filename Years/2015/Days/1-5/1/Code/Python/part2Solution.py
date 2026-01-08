189bcf42d95eba8014da7a5b54a73e9ad509e9c0
IoT4Env <170166502+IoT4Env@users.noreply.github.com>
2026-01-08 07:52:45 +0100

    Solved day1 part 2 year 2015 in Python

--- Years/2015/Days/1-5/1/Code/Python/part2Solution.py
+++ Years/2015/Days/1-5/1/Code/Python/part2Solution.py
@@ -8,21 +8,24 @@ depth = int(sys.argv[2])
 sys.path.append('../' * depth)
 from Languages.Python import read_file, FileExtensions
 
-def solve_part1(input1: str) -> int:
+def solve_part2(input1: str) -> int:
 	input1_lines: list[str] = read_file(input1, FileExtensions.TXT)
 
 	current_floor: int = 0
+	
 	# For "one-line" input files
 	if len(input1_lines) == 1:
 		innput_single = input1_lines[0]
-		
-		move_up_count = sum((1 for c in innput_single if c == '('))
-		move_down_count = sum((1 for c in innput_single if c == ')'))
-		current_floor = move_up_count - move_down_count
-		return current_floor
+		for i, c in enumerate(innput_single, 1):
+			current_floor += 1 if c == '(' else -1
+			if current_floor == -1:
+				return i
+	
+	raise Exception("Provided input does not bring you to the basement at any point!")
+
 
 def main() -> None:
-	solution = solve_part1(str(sys.argv[1]))
+	solution = solve_part2(str(sys.argv[1]))
 	print(solution)
 
 
